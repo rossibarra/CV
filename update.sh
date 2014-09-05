3e87941bd771d2497ced0c816d282250c47e66ca
Jeffrey Ross-Ibarra <rossibarra@gmail.com>
2014-09-05 00:23:46 -0700

    update

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-perl -e '$bob=`rscript --vanilla  h.r`; $bob=~s/\\//; $bob=~s/\[1\]//;$bob=~s/\"//g;  open FILE, "<CV.temp.tex"; open CV, ">CV.tex"; while(<FILE>){ if($_=~m/^H\-Index/){ print CV "$bob\n" } else{ print CV $_;}    }'
+perl update.pl
 pdflatex CV.tex
 
 git commit -am "update";
