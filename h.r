7a62766dfa59ee5bae8af9c242407bfa2080b654
Jeffrey Ross-Ibarra <rossibarra@gmail.com>
2014-09-05 00:21:18 -0700

    update

--- h.r
+++ h.r
@@ -3,3 +3,5 @@
 library(scholar)
 bob<-get_profile("5SzRq1oAAAAJ")
 paste("\\small{H-Index ", bob$h_index, " (", bob$total_cites, " citations as of ", date(), ")}",sep="")
+bob<-get_publications("5SzRq1oAAAAJ")
+write.table(file="./tempcites.txt",data.frame(bob$id,bob$cites),row.names=F,quote=F,col.names=F)
