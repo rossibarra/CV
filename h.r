ac5b52cab6aedb5dbc15e26b76b20f30311af160
Jeff Ross-Ibarra <rossibarra@gmail.com>
2016-06-21 01:19:36 -0700

    update

--- h.r
+++ h.r
@@ -1,5 +1,6 @@
 # echo $( rscript --vanilla  h.r ) | sed -e 's/\[1\]//g;s/\"//g'
 
+library(methods)
 library(scholar)
 bob<-get_profile("5SzRq1oAAAAJ")
 paste("{\\small H-Index ", bob$h_index, " (", bob$total_cites, " citations as of ", date(), ")}",sep="")
